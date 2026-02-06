b89d737e830c0b53afdf82c21973f03974071392
w.i <w.i@example.com>
2026-02-06 14:32:46 +0200

    Update base path for gehadservice

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/sa3edservice/',
+    base: '/gehadservice/',
   
   plugins: [react()],
   optimizeDeps: {
